f593e8f18f78e5e3aafebd1e9044e6ed9cadff24
Nitin Kansal <31030410+nitinkansal1984@users.noreply.github.com>
2022-05-31 14:47:20 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "resource_group_name" {
+  default       = "myResourceGroup"
+}
+
+variable "resource_group_location" {
+  default       = "eastus"
+  description   = "Location of the resource group."
+}
+
+variable "agent_count" {
+    default = 1
+}
+
+variable "ssh_public_key" {
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+    default = "myAKSCluster"
+}
+
+variable cluster_name {
+    default = "myAKSCluster"
+}
+
+## In case you are using service prinicipal to authenticate to resource provider
+variable aks_service_principal_app_id {
+}
+
+variable aks_service_principal_client_secret {
+}
