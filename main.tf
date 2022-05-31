a1d956a56cfd1a0c3dc1e41a997628c089946219
Nitin Kansal <31030410+nitinkansal1984@users.noreply.github.com>
2022-05-31 14:46:55 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "azurerm_kubernetes_cluster" "k8s" {
+    name                = var.cluster_name
+    location            = var.resource_group_location
+    resource_group_name = var.resource_group_name
+    dns_prefix          = var.dns_prefix
+
+    linux_profile {
+        admin_username = "ubuntu"
+
+        ssh_key {
+            key_data = file(var.ssh_public_key)
+        }
+    }
+
+    default_node_pool {
+        name            = "agentpool"
+        node_count      = var.agent_count
+        vm_size         = "Standard_D2_v2"
+    }
+
+# There are many ways to authenticate to the Azure provider. In this demo we will use service principal.
+
+    service_principal {
+        client_id     = var.aks_service_principal_app_id
+        client_secret = var.aks_service_principal_client_secret
+    }
+
+# In case you want to use system managed identity
+
+#   identity {
+#       type = "SystemAssigned"
+#  }
+
+    network_profile {
+        load_balancer_sku = "Standard"
+        network_plugin = "kubenet"
+    }
+
+    tags = {
+        Environment = "Development"
+    }
+}
