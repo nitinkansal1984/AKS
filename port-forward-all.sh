3424f713d8c6ade1b8a1124713d6b0531b1b66e6
Nitin Kansal <31030410+nitinkansal1984@users.noreply.github.com>
2022-06-07 15:43:51 +0530

    Update port-forward-all.sh

--- port-forward-all.sh
+++ port-forward-all.sh
@@ -6,23 +6,21 @@ export BOOKSTORE_NAMESPACE=bookstore
 export BOOKWAREHOUSE_NAMESPACE=bookwarehouse
 
 ## forward bookbuyer port
-BOOKBUYER_LOCAL_PORT="${BOOKBUYER_LOCAL_PORT:-8080}"
+BOOKBUYER_LOCAL_PORT="${BOOKBUYER_LOCAL_PORT:-7070}"
 POD="$(kubectl get pods --selector app=bookbuyer -n "$BOOKBUYER_NAMESPACE" --no-headers  | grep 'Running' | awk 'NR==1{print $1}')"
-kubectl port-forward "$POD" -n "$BOOKBUYER_NAMESPACE" "$BOOKBUYER_LOCAL_PORT":14001
+kubectl port-forward "$POD" -n "$BOOKBUYER_NAMESPACE" "$BOOKBUYER_LOCAL_PORT":14001 &
 
 ## forward bookstore version 1
-BOOKSTOREv1_LOCAL_PORT="${BOOKSTOREv1_LOCAL_PORT:-8081}"
-POD="$(kubectl get pods --selector app="$backend" -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
-kubectl port-forward "$POD" -n "$BOOKSTORE_NAMESPACE" "$BOOKSTOREv1_LOCAL_PORT":14001
+BOOKSTOREv1_LOCAL_PORT="${BOOKSTOREv1_LOCAL_PORT:-7071}"
+POD="$(kubectl get pods --selector app=bookstore-v1 -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
+kubectl port-forward "$POD" -n "$BOOKSTORE_NAMESPACE" "$BOOKSTOREv1_LOCAL_PORT":14001 &
 
 ## forward bookstore version 2
-BOOKSTOREv2_LOCAL_PORT="${BOOKSTOREv2_LOCAL_PORT:-8082}"
-POD="$(kubectl get pods --selector app="$backend" -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
-kubectl port-forward "$POD" -n "$BOOKSTORE_NAMESPACE" "$BOOKSTOREv2_LOCAL_PORT":14001
+BOOKSTOREv2_LOCAL_PORT="${BOOKSTOREv2_LOCAL_PORT:-7072}"
+POD="$(kubectl get pods --selector app=bookstore-v2 -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
+kubectl port-forward "$POD" -n "$BOOKSTORE_NAMESPACE" "$BOOKSTOREv2_LOCAL_PORT":14001 &
 
 ## forward bookthief 
-BOOKTHIEF_LOCAL_PORT="${BOOKTHIEF_LOCAL_PORT:-8083}"
+BOOKTHIEF_LOCAL_PORT="${BOOKTHIEF_LOCAL_PORT:-7073}"
 POD="$(kubectl get pods --selector app=bookthief -n "$BOOKTHIEF_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
-kubectl port-forward "$POD" -n "$BOOKTHIEF_NAMESPACE" "$BOOKTHIEF_LOCAL_PORT":14001
-
-
+kubectl port-forward "$POD" -n "$BOOKTHIEF_NAMESPACE" "$BOOKTHIEF_LOCAL_PORT":14001 &
